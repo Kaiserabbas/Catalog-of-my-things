01c34fa3bd88781c2367ae61ee87f22481837e07
Qaisar <kayser.abbas@gmail.com>
2023-10-24 09:19:52 +0500

    Add options : List all books List all labels (e.g. 'Gift', 'New') Add a book

--- main.rb
+++ main.rb
@@ -1,40 +1,48 @@
-require_relative 'item' 
-items = [
-  Item.new(1, 'Mystery', 'Agatha Christie', 'Library', 'HarperCollins', Time.new(2010, 1, 15), false),
-  Item.new(2, 'Science Fiction', 'Isaac Asimov', 'Bookstore', 'Penguin', Time.new(2005, 5, 20), false),
-  # Add more items as needed
-]
+require_relative 'book'  # Include the Book class defined in book.rb
+require_relative 'label' # Include the Label class defined in label.rb
+
+# Create an array to store books and labels
+books = []
+labels = []
 
 def print_options
   puts "Options:"
-  puts "1. List Items"
-  puts "2. Move an Item to Archive"
-  puts "3. Quit"
+  puts "1. List all books"
+  puts "2. List all labels"
+  puts "3. Add a book"
+  puts "4. Quit"
 end
 
 loop do
-  puts "Welcome to the Item Management App"
+  puts "Welcome to the Library Management App"
   print_options
   print "Enter your choice: "
   choice = gets.chomp.to_i
 
   case choice
   when 1
-    puts "List of Items:"
-    items.each do |item|
-      puts "Item ID: #{item.id}, Genre: #{item.genre}, Author: #{item.author}, Published Date: #{item.publish_date}, Archived: #{item.archived ? 'Yes' : 'No'}"
+    puts "List of Books:"
+    books.each do |book|
+      puts "ID: #{book.id}, Author: #{book.first_name} #{book.last_name}"
     end
   when 2
-    puts "Enter the ID of the item to move to the archive:"
-    item_id = gets.chomp.to_i
-    item = items.find { |i| i.id == item_id }
-    if item
-      item.move_to_archive
-      puts "Item with ID #{item_id} has been moved to the archive."
-    else
-      puts "Item not found with the given ID."
+    puts "List of Labels:"
+    labels.each do |label|
+      puts "ID: #{label.id}, Title: #{label.title}, Color: #{label.color}"
     end
   when 3
+    puts "Add a Book:"
+    print "Enter Author's First Name: "
+    first_name = gets.chomp
+    print "Enter Author's Last Name: "
+    last_name = gets.chomp
+
+    # Create a new book and add it to the list of books
+    book = Book.new(books.length + 1, first_name, last_name)
+    books << book
+
+    puts "Book added successfully!"
+  when 4
     puts "Goodbye!"
     break
   else
