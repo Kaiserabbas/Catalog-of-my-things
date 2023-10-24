8495849ebc84d9e75e6113a6f3fe6a3b104cd796
Qaisar <kayser.abbas@gmail.com>
2023-10-24 10:21:11 +0500

    Modify book.rb to to add instance variable cover_state

--- book.rb
+++ book.rb
@@ -1,29 +1,27 @@
 # book.rb
 
-class Book
-  attr_accessor :id, :first_name, :last_name
-  attr_reader :items
+require_relative 'item'  # Include the Item class defined in item.rb
+class Book < Item
+  attr_accessor :cover_state
+  attr_reader :items  # Define the items attribute
 
-  def initialize(id, first_name, last_name)
-    @id = id
-    @first_name = first_name
-    @last_name = last_name
-    @items = []  # Initialize an empty array to hold associated items
+  def initialize(id, first_name, last_name, cover_state)
+    super(id, "Book", "#{first_name} #{last_name}", nil, nil, nil, false)
+    @cover_state = cover_state
+    @items = []  # Initialize @items as an empty array
+  end
+
+  def can_be_archived?
+    super || @cover_state == "bad"
   end
 
   def add_item(item)
     @items << item
+    item.label = self  # Use the setter method in the Item class
   end
 
   def remove_item(item)
     @items.delete(item)
-  end
-
-  def can_be_archived?
-    if super || @cover_state == "bad"
-      return true
-    else
-      return false
-    end
+    item.label = nil  
   end
 end
